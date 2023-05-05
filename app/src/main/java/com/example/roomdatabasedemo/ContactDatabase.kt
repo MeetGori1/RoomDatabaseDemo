8a8734c7c3edb4cf6e7224b2bbe35da20591aa76
meet <meet.devstree@gmail.com>
2023-05-05 12:00:16 +0530

    - added database and it's functionlity

---
+++ app/src/main/java/com/example/roomdatabasedemo/ContactDatabase.kt
@@ -0,0 +1,10 @@
+package com.example.roomdatabasedemo
+
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.RoomDatabase
+@Database(entities = [Contact::class],version=1)
+abstract class ContactDatabase :RoomDatabase(){
+
+    abstract fun ContactDao():ContactDAO
+}
--- app/src/main/java/com/example/roomdatabasedemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabasedemo/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.roomdatabasedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
+import com.example.roomdatabasedemo.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var database: ContactDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding=ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        // this is not ideal way , kindly refer singleton pattern
+        database=Room.databaseBuilder(applicationContext,ContactDatabase::class.java,"contactDB").build()
+
+        GlobalScope.launch {
+            database.ContactDao().insertContact(Contact(0,"meet","12345678980"))
+        }
+
+        binding.text.setOnClickListener {
+            database.ContactDao().getContact().observe(this,{
+                binding.text.text=it[1].name
+            })
+        }
     }
 }
